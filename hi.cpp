51dbc552bf474be561bd7fc29a599643e365cceb
zakiyu5 <ssentongozakiyu@gmail.com>
2024-10-05 11:41:29 +0530

    first commit

---
+++ hi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int a = 5;
+    int c = a*a;
+    cout <<c;
+}
--- wifichecker.py
+++ wifichecker.py
@@ -3,4 +3,3 @@ import subprocess
 nw = subprocess.check_output(["netsh","wlan","show","network"])
 decoded_nw = nw.decode("ascii")
 print(decoded_nw)
-#print(decorded_nw)<|eom_id|><|start_header_id|>assistant
